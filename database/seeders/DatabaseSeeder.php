8dd9024a52d71a44ced45c5b80e8bfd2205444ee
vgianluca96 <gianluca.vallese@hotmail.com>
2023-10-30 16:54:32 +0100

    aggiornato Database Seeder con Faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,19 +4,28 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Train;
+use Faker\Generator as Faker;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run(Faker $faker)
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        for ($i = 0; $i < 10; $i++) {
+            $newtrain = new Train();
+            $newtrain->company = $faker->randomElement(['Trenitalia S.p.A', 'Italo S.p.A']);
+            $newtrain->departure_station = $faker->randomElement(['Venezia', 'Padova', 'Rovigo', 'Brescia', 'Bergamo', 'Milano', 'Roma', 'Napoli', 'Torino', 'Firenze']);
+            $newtrain->arrival_station = $faker->randomElement(['Venezia', 'Padova', 'Rovigo', 'Brescia', 'Bergamo', 'Milano', 'Roma', 'Napoli', 'Torino', 'Firenze']);
+            $newtrain->departure_hour = $faker->time();
+            $newtrain->arrival_hour = $faker->time();
+            $newtrain->train_code = $faker->bothify('??######');
+            $newtrain->wagons_num = $faker->numberBetween(2, 20);
+            $newtrain->on_time = $faker->boolean();
+            $newtrain->canceled = $faker->boolean();
+            $newtrain->save();
+        }
     }
 }
