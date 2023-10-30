3d257a86dd3f8ea3ce9882474cf40aa8a6c954a5
vgianluca96 <gianluca.vallese@hotmail.com>
2023-10-30 17:13:42 +0100

    stampa dei treni in pagina

--- app/Http/Controllers/Guests/UserController.php
+++ app/Http/Controllers/Guests/UserController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\Guests;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Train;
 
 class UserController extends Controller
 {
     public function index()
     {
-        return view('home');
+
+        $trains = Train::all();
+
+        return view('home', compact('trains'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,6 +19,42 @@
 
     <div class="container py-4">
         <h1>Hello</h1>
+
+        
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Azienda</th>
+                <th scope="col">Stazione di partenza</th>
+                <th scope="col">Stazione di arrivo</th>
+                <th scope="col">Orario di partenza</th>
+                <th scope="col">Orario di arrivo</th>
+                <th scope="col">Codice Treno</th>
+                <th scope="col">Numero Carrozze</th>
+                <th scope="col">In orario</th>
+                <th scope="col">Cancellato</th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach ($trains as $train)
+                    <tr>
+                        <th scope="row">{{$train->id}}</th>
+                        <td>{{$train->company}}</td>
+                        <td>{{$train->departure_station}}</td>
+                        <td>{{$train->arrival_station}}</td>
+                        <td>{{$train->departure_hour}}</td>
+                        <th>{{$train->arrival_hour}}</th>
+                        <td>{{$train->train_code}}</td>
+                        <td>{{$train->wagons_num}}</td>
+                        <th>{{$train->on_time}}</th>
+                        <td>{{$train->canceled}}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+          </table>
+    
+        
     </div>
 
 </body>
